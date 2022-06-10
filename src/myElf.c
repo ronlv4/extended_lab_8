6e56a77a4d86da2678d0d15c3f57c978b9da9a9c
Ron Levi <ron7@post.bgu.ac.il>
2022-06-10 11:45:21 +0300

    working examine

--- src/myElf.c
+++ src/myElf.c
@@ -1,20 +1,33 @@
 #include "stdlib.h"
 #include "stdio.h"
 #include "string.h"
-#include "ctype.h"
 #include "unistd.h"
+#include "ctype.h"
+#include "elf.h"
+#include "errno.h"
+#include "fcntl.h"
 #include "sys/types.h"
 #include "sys/stat.h"
-#include "fcntl.h"
-#include "sys/mman.h"
-#include "errno.h"
 #include "sys/stat.h"
-#include "elf.h"
+#include "sys/mman.h"
+#include "sys/ioctl.h"
 
 #define MAX_FILE_NAME_LENGTH 1024
+#define COL_WIDTH 30
 
-int DEBUG = 0;
-int current_fd = -1;
+struct state {
+    int debug_mode;
+    int current_fd;
+    void *map_start;
+    Elf32_Ehdr *elf_header;
+    struct stat fd_stat;
+} s = {
+        0,
+        -1,
+        NULL,
+        NULL,
+        {0}
+};
 
 struct fun_desc {
     char *name;
@@ -23,63 +36,118 @@ struct fun_desc {
 };
 
 void toggle_debug_mode();
+
 void examine_elf_file();
+
 void print_section_names();
+
 void print_symbols();
+
 void quit();
+
 void print_menu();
+
 void parse_input();
+
 void flush();
+
 void remove_ending_newline();
 
+void reset_current_fd();
+
+void print_column_data_int(int, int, int );
+
+void print_column_data_str(int, int, char*);
+
+//void print_padded_column_data();
+
 void toggle_debug_mode() {
-    if (DEBUG) {
+    if (s.debug_mode) {
+        s.debug_mode = 0;
         printf("Debug flag is now off\n");
-        DEBUG = 0;
     } else {
-        DEBUG = 1;
+        s.debug_mode = 1;
         printf("Debug flag is now on\n");
     }
 }
 
 void examine_elf_file() {
-    Elf32_Ehdr * header;
-    void * map_start;
     printf("elf file name: ");
     char *elf_file_name = malloc(MAX_FILE_NAME_LENGTH * sizeof(char));
-    scanf("%s", elf_file_name);
-    if (current_fd != -1) {
-        close(current_fd);
+//    scanf("%s", elf_file_name);
+    elf_file_name = "abc";
+    if (s.current_fd != -1) {
+        if (close(s.current_fd) == -1) {
+            perror("could not close current file descriptor");
+            reset_current_fd();
+            exit(errno);
+        }
     }
-    current_fd = open(elf_file_name, O_RDONLY);
-    if (current_fd == -1) {
+//    printf("read file %s\n", elf_file_name);
+    s.current_fd = open(elf_file_name, O_RDONLY);
+//    printf("opened %s\n", elf_file_name);
+    if (s.current_fd == -1) {
         perror("could not open elf file");
+        reset_current_fd();
         exit(errno);
     }
-    struct stat fd_stat;
-
-    if( fstat(current_fd, &fd_stat) != 0 ) {
+    if (fstat(s.current_fd, &s.fd_stat) != 0) {
         perror("stat failed");
-        exit(-1);
+        reset_current_fd();
+        exit(errno);
     }
-    if ( (map_start = mmap(0, fd_stat.st_size, PROT_READ , MAP_SHARED, current_fd, 0)) == MAP_FAILED ) {
+//    printf("read stats %s\n", elf_file_name);
+
+    if ((s.map_start = mmap(0, s.fd_stat.st_size, PROT_READ, MAP_SHARED, s.current_fd, 0)) == MAP_FAILED) {
         perror("mmap failed");
-        exit(-4);
+        reset_current_fd();
+        exit(errno);
     }
 //    write(STDOUT_FILENO, p, 100);
-    header = (Elf32_Ehdr *) map_start;
-    printf("ELF Header:\n");
-    printf("  %-8s:%-3X%-3X%-3X%-3X\n" ,"Magic",header->e_ident[EI_MAG0], header->e_ident[EI_MAG1], header->e_ident[EI_MAG2], header->e_ident[EI_MAG3]);
+    s.header = (Elf32_Ehdr *) s.map_start;
+    print_column_data_str(0, COL_WIDTH, "Elf Header");
+//    printf("ELF Header:\n");
+    printf("  %s:%3x%3x%3x\n", "Magic", s.header->e_ident[EI_MAG0], s.header->e_ident[EI_MAG1],
+           s.header->e_ident[EI_MAG2]);
+    // TODO: print data encoding scheme
+    printf("Entry point address: %#10x\n", s.header->e_entry);
+    print_column_data_str(3, COL_WIDTH, "Start of section headers:");
+    print_column_data_int(0, COL_WIDTH, s.header->e_shoff);
+    print_column_data_str(3, COL_WIDTH, "Number of section headers:");
+    print_column_data_int(0, COL_WIDTH, s.header->e_shnum);
+    print_column_data_str(3, COL_WIDTH, "Size of section headers:");
+    print_column_data_int(0, COL_WIDTH, s.header->e_shentsize);
+    print_column_data_str(3, COL_WIDTH, "Start of program headers:");
+    print_column_data_int(0, COL_WIDTH, s.header->e_phoff);
+    print_column_data_str(3, COL_WIDTH, "Number of program headers:");
+    print_column_data_int(0, COL_WIDTH, s.header->e_phnum);
+    print_column_data_str(3, COL_WIDTH, "Size of program headers:");
+    print_column_data_int(0, COL_WIDTH, s.header->e_phentsize);
 }
 
-void remove_ending_newline(char *str){
+void reset_current_fd() {
+    munmap(s.map_start, s.fd_stat.st_size);
+    close(s.current_fd);
+    s.current_fd = -1;
+}
+
+void remove_ending_newline(char *str) {
     int size = 0;
-    while (str[size++]){}
+    while (str[size++]) {}
     str[size - 2] = '\0';
 }
 
 void print_section_names() {
-    printf("not implemented yed");
+    if (s.current_fd == -1){
+        printf("There is no elf file currently opened");
+        return;
+    }
+
+
+    Elf32_Shdr current_section;
+
+
+
 }
 
 void print_symbols() {
@@ -116,6 +184,24 @@ void flush() {
     while ((c = fgetc(stdin)) != '\n' && c != EOF) {
         printf("flushing %c\n", c);
     }
+}
+
+
+
+void print_column_data_int(int offset, int data_width, int data) {
+    while (offset--)
+        printf(" ");
+    printf("%-*d\n", data_width, data);
+}
+
+void print_column_data_str(int offset, int data_width, char *data) {
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+//    size_t data_length = strlen(data);
+    while (offset--)
+        printf(" ");
+    printf("%-*s", data_width, data);
+
 }
 
 int main(int argc, char **argv) {
